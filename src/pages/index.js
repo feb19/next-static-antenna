dd6fc022f5e7056f7635fda2c526575813f59b56
feb19 <nt@feb19.jp>
2021-06-19 11:26:46 +0900

    add timezone

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,11 @@
 import data from "../../output.json"
 import dayjs from "dayjs"
+import utf from "dayjs/plugin/utc"
+import timezone from "dayjs/plugin/timezone"
 import 'dayjs/locale/ja'
 
+dayjs.extend(utf)
+dayjs.extend(timezone)
 dayjs.locale('ja')
 
 function Antenna({rss}) {
@@ -10,7 +14,7 @@ function Antenna({rss}) {
     <ul>{
       rss.data.items.slice(0,4).map((data) => {
         return <div key={data.link}>
-          <li><a href={data.link} target="_blank">{data.title} - {dayjs(Date(data.isoDate)).format('YYYY/MM/DD')}</a></li>
+          <li><a href={data.link} target="_blank">{data.title} - {dayjs(Date(data.isoDate)).tz("Asia/Tokyo").format('YYYY/MM/DD')}</a></li>
         </div>
       })
     }</ul>
@@ -26,7 +30,7 @@ function Index({ rss, buildTime }) {
         <Antenna key={rssData.url} rss={rssData} />
       </div>
     })}
-    <div>fetch at:（{dayjs(Date(buildTime)).format('YYYY/MM/DD HH:mm:ss')}）</div>
+    <div>fetch at:（{dayjs(Date(buildTime)).tz("Asia/Tokyo").format('YYYY/MM/DD HH:mm:ss')}）</div>
   </>
 }
 
